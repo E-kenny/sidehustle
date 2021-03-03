6788ec29b08507c194044638f440e7a0e16f6ebd
Ekene <ekennyobiasogu@gmail.com>
2021-03-03 12:24:54 +0100

    my authentication task

---
+++ authentication/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+    div {
+        margin : auto;
+        width: 30%;
+        border-radius: 20px;
+        height: 70vh;
+    }
+    input[type='text'], input[type='password'], input[type='email']{
+        width:90%;
+        border: 2px solid grey;
+        border-radius: 30px;
+        margin: auto;
+    }
+    
+    </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <h1>Log In</h1>
+    <form action="index.php" method="post">
+        <label for="">Username:</label><br><input type="text" name="username" id="" required><br>
+        <br>
+        <label for="">Password:</label><br><input type="password" name="password" id="" required><br>
+        <br>
+        <input type="submit" value="submit">
+    </form>
+    <h4>Don't have account yet? <a href="signUp.php">go to Sign up</a></h4>
+    </div>
+</body>
+</html>
---
+++ authentication/signUp.php
@@ -0,0 +1,50 @@
+<?php 
+if(isset($_POST['submit'])){
+    session_start();
+$_SESSION['username'] = htmlspecialchars($_POST['username']) ;
+$_SESSION['password'] = htmlspecialchars($_POST['password']) ;
+
+header("location: login.php");
+
+} else {
+
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+    div {
+        margin : auto;
+        width: 30%;
+        border-radius: 20px;
+        height: 70vh;
+    }
+    input[type='text'], input[type='password'], input[type='email']{
+        width:90%;
+        border: 2px solid grey;
+        border-radius: 30px;
+        margin: auto;
+    }
+    
+    </style>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+    <h1>Sign Up</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+        <label for="">Username:</label><br><input type="text" name="username" id="" required><br>
+        <br>
+        <label for="">Password:</label><br><input type="password" name="password" id="" required><br>
+        <br>
+        <input type="submit" name="submit" value="submit">
+    </form>
+    <h4>Already Signed up? <a href="login.php">go to login</a></h4>
+    </div>
+</body>
+</html>
+<?php
+}
