14103acc413ab8570356545c2ac0fd07f4d8dc25
Ekene <ekennyobiasogu@gmail.com>
2021-04-22 09:49:47 +0100

    message

--- calculator/index.php
+++ calculator/index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Basic PHP Calculator</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="UTF-8">
+
+<title>Calculator</title>
 </head>
 <body>
 <?php
